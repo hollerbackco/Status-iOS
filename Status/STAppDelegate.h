2888c43d063bf580fa433c00dca78744b000ba6d
Joe Nguyen <isthisjoe@gmail.com>
2014-05-19 14:32:43 -0400

    Initial Commit

---
+++ Status/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  Status
+//
+//  Created by Joe Nguyen on 19/05/2014.
+//  Copyright (c) 2014 Status. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Status/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Status
+//
+//  Created by Joe Nguyen on 19/05/2014.
+//  Copyright (c) 2014 Status. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
